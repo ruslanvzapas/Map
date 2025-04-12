63d5d5ae1f28f89d5ab692891e62cd56b2ae01ae
Ruslan Vavulskyi-Zapasnyk <124261674+ruslanvzapas@users.noreply.github.com>
2025-04-12 13:15:24 +0300

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Ruslan Vavulskyi-Zapasnyk on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
